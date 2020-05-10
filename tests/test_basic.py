598da1230a93c0c64e0d2710f27b994a3ca127b8
Matheus Bratfisch <matheusbrat@gmail.com>
2020-05-10 13:45:56 -0300

    Add test for depth=2 (#5)

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -52,3 +52,36 @@ def test_basic(no_cyclic_references):
         assert "secret123" not in list(locals.values())
 
     no_cyclic_references(f)
+
+
+def test_basic_depth_2(no_cyclic_references):
+    def f():
+        is_test_func = True  # noqa
+
+        def _login_user(username, password):
+            is_inside_nested_func = True  # noqa
+            print("logging in " + username + password)
+
+        @sensitive_variables("password", depth=2)
+        def login_user(username, password):
+            is_inside_func = True  # noqa
+            _login_user(username, password)
+
+        try:
+            login_user(None, "secret123")
+        except TypeError:
+            test_locals, wrapper_locals, login_user_locals, locals = get_all_variables()
+        else:
+            assert False
+
+        # Assert that we got the right frames
+        assert test_locals["is_test_func"]
+        assert locals["is_inside_nested_func"]
+        assert wrapper_locals["f"]
+        assert login_user_locals["_login_user"]
+
+        # Assert real functionality
+        assert locals["password"] == PLACEHOLDER
+        assert "secret123" not in list(locals.values())
+
+    no_cyclic_references(f)
